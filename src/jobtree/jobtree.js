77b54ef3112b7b2ecd31c8b7d8c20ac4a689905a
chenfeng <cfeng690@gmail.com>
2018-05-01 09:42:50 +0800

    开发完成版本

--- src/jobtree/jobtree.js
+++ src/jobtree/jobtree.js
@@ -1,42 +1,51 @@
 import React, { Component } from "react";
 import './jobtree.css';
 
+
 // 招聘组件 - 通用复选框
 class JobCheckbox extends Component {
   render() {
-    let checkState = 'default';
-
     return (
-      <div className={'checkbox ' + checkState}>
-        <input type="checkbox" />
+      <div className={'checkbox ' + this.props.checkState}
+        onClick={this.props.clickChange}
+      >
+        <div></div>
       </div>
     );
   }
 }
+
 
 // 招聘组件 - 部门 - 头部
 class JobTreeDepartHeader extends Component {
   render() {
-    const name = this.props.name;
-
+    const { name, checkState, total } = this.props;
     return (
       <div className="depart-header sub-color">
-        <JobCheckbox /> 
+        <JobCheckbox 
+          clickChange={this.props.checkboxClick}
+          checkState={checkState}
+        /> 
         <span className="name">{name}</span>
-        <span className="number">0</span>
+        <span className="icon"></span>
+        <span className="number">{total}</span>
       </div>
     );
   }
 }
 
-// 招聘组件 - 部门 - 职位列表
+
+// 招聘组件 - 部门 - 职位列表项
 class JobTreeItem extends Component {
   render() {
-    const { name, number, checked } = this.props.job;
-
+    const { name, number } = this.props.job;
+    const checkState = this.props.checkState || 'null';
     return (
       <li>
-        <JobCheckbox />
+        <JobCheckbox 
+          clickChange={this.props.checkboxClick}
+          checkState={checkState}
+        />
         <span>{name}</span>
         <span className="number">{number}</span>
       </li>
@@ -44,21 +53,108 @@ class JobTreeItem extends Component {
   }
 }
 
+
 // 招聘组件 - 部门
 class JobTreeDepartment extends Component {
   state = {
-    department: this.props.departJobs
+    department: Object.assign({}, this.props.departJobs),
+    departmentCheckState: null
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      departmentCheckState: this.setDepartCheckboxState(this.state.department.children)
+    })
+  }
+
+  // 单击职位头部复选框触发事件
+  headerCheckboxChange = (clearCmd) => {
+    let listChecked;
+    if(clearCmd && clearCmd === 'clear') {
+      listChecked = false;
+    } else {
+      let departmentCheckState = this.state.departmentCheckState;
+      switch (departmentCheckState) {
+        case 'all':
+          listChecked = false;
+          break;
+        case 'part':
+        case 'null':
+          listChecked = true;
+      }
+    }
+
+    this.setState((prevState) => {
+      const nextState = {...prevState};
+      const children = nextState.department.children;
+      children.forEach( item => {
+        item.checked = listChecked;
+      });
+      nextState.departmentCheckState = this.setDepartCheckboxState(children);
+      return nextState;
+    });
+  }
+
+  // 单击职位列表项的复选框时触发的事件
+  listCheckboxChange = (key) => {
+    const items = this.state.department.children;
+    const idx = items.findIndex( item => {
+      return item.key === key;
+    });
+    this.setState(( prevState ) => {
+      const nextState = {...prevState};
+      const nextChildren = nextState.department.children;
+      nextChildren[idx].checked = !prevState.department.children[idx].checked;
+      nextState.departmentCheckState = this.setDepartCheckboxState(nextChildren);
+      return nextState;
+    })
+  }
+
+  // 设置部门复选框选中状态
+  setDepartCheckboxState = (items) => {
+    let departmentCheckState;
+    if (items.every( job => {
+      return job.checked;
+    })) {
+      departmentCheckState = 'all';
+    } else if (items.every( job => {
+      return !job.checked;
+    })) {
+      departmentCheckState = 'null';
+    } else {
+      departmentCheckState = 'part';
+    }
+    return departmentCheckState;
+  }
+
+  // 清空所有复选框
+  clear = () => {
+    this.headerCheckboxChange('clear');
   }
 
   render() {
-    const { name, children } = this.state.department;
-    const items = children.map( job => (
-      <JobTreeItem key={job.key} job={job} />
-    ));
+    const { departmentCheckState, department: {name, children} } = this.state;
+    let total = 0;
+    const items = children.map( job => {
+      if (job.checked) {
+        total += job.number;
+      }
+      return <JobTreeItem 
+        key={job.key} 
+        job={job} 
+        checkState={ job.checked ? 'all' : 'null' }
+        checkboxClick={ this.listCheckboxChange.bind(this, job.key) }
+      />
+    });
 
     return (
       <div className="department">
-        <JobTreeDepartHeader name={name} />
+        <JobTreeDepartHeader 
+          name={name} 
+          total={total}
+          checkboxClick={ this.headerCheckboxChange }
+          checkState={ departmentCheckState }
+        />
         <ul>{items}</ul>
       </div>
     )
@@ -68,19 +164,33 @@ class JobTreeDepartment extends Component {
 
 // 招聘组件 - 主体
 class JobTreeBody extends Component {
-  state = {
-    jobs: this.props.jobs
+  constructor(props) {
+    super(props);
+    this.departments = [];
+  }
+
+  // 清空所有复选框
+  clear = () => {
+    this.departments.forEach( depart => {
+      depart.current.clear();
+    });
   }
   
   render() {
     const departments = [],
-      jobs = this.state.jobs;
+      jobs = this.props.jobs;
 
     jobs.forEach(department => {
+      const ref = React.createRef();
+      this.departments.push(ref);
+
       departments.push(
-        <JobTreeDepartment departJobs={department} />
+        <JobTreeDepartment
+          departJobs={department} 
+          ref={ref}
+        />
       );
-    });
+    }, this);
 
    return (
     <div className="body">
@@ -97,7 +207,11 @@ class JobTreeHeader extends Component {
     return (
       <header>
         <h2>招聘职位</h2>
-        <div className="btn sub-color">清空</div>
+        <div className="btn sub-color"
+          onClick={this.props.clear}
+        >
+          清空
+        </div>
       </header>
     )
   }
@@ -106,11 +220,23 @@ class JobTreeHeader extends Component {
 
 // 招聘组件
 class JobTree extends Component {
+  constructor(props) {
+    super(props);
+    this.jobTreeBody = React.createRef();
+  }
+
+  clear = () => {
+    this.jobTreeBody.current.clear();
+  }
+
   render() {
     return (
       <div className="jobtree">
-        <JobTreeHeader />
-        <JobTreeBody jobs={this.props.jobs} />
+        <JobTreeHeader clear={this.clear} />
+        <JobTreeBody 
+          jobs={this.props.jobs} 
+          ref={this.jobTreeBody}
+        />
       </div>
     );
   }
